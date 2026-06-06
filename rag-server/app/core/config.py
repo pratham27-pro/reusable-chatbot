9f9444c58c1e0d7f362c44bcc01d48696fe419ea
Pratham Jain <2muchappy420@gmail.com>
2026-06-06 10:35:13 +0530

    server typo fixed

--- rag-server/app/core/config.py
+++ rag-server/app/core/config.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     groq_api_key: SecretStr
     pinecone_api_key: SecretStr
-    pinecone_index_name: str = "reusbale-chatbot"
-    pinecone_host: str = ""          # ← the host URL Pinecone gave you
+    pinecone_index_name: str = "reusable-chatbot"
+    pinecone_host: str
 
     class Config:
         env_file = ".env"
